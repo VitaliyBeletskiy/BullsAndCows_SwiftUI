407ea764a9c42aa520210a958a276dc367303898
Vitaliy Beletskiy <vitaliy.beletskiy@gmail.com>
2020-11-19 20:14:52 +0100

    Initial Commit

---
+++ BullsAndCows/BullsAndCowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsAndCowsApp.swift
+//  BullsAndCows
+//
+//  Created by Vitaliy on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct BullsAndCowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
